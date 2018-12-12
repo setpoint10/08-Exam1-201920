b7cc6fd5262b103c45e0a40a1cab8dba89b56afb
xiny <xiny@rose-hulman.edu>
2018-12-11 20:42:10 -0500

    done

--- src/problem3.py
+++ src/problem3.py
@@ -3,8 +3,8 @@ Exam 1, problem 3.
 
 Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher,
          Mark Hays, Amanda Stouder, Aaron Wilkin, their colleagues,
-         and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         and Yu Xin.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import testing_helper
 import time
@@ -17,7 +17,7 @@ def main():
 
 
 ###############################################################################
-# TODO: 2.  READ the doc-string for the  sum_of_digits  function defined below.
+# DONE: 2.  READ the doc-string for the  sum_of_digits  function defined below.
 # It is the same  sum_of_digits  function that you have seen before.
 # After you UNDERSTAND the doc-string (JUST the doc-string, NOT the code),
 # ASKING QUESTIONS AS NEEDED, change the above _TODO_ to DONE.
@@ -137,6 +137,13 @@ def run_test_problem3a():
 
 
 def problem3a(r, s):
+    total=0
+    for k in range(r,s+1):
+        if sum_of_digits(k ** 3)%2 == 1:
+            total=total+k
+    return total
+
+
     """
     What comes in:  Positive integers r and s, with r <= s.
     What goes out:
@@ -163,7 +170,7 @@ def problem3a(r, s):
           -- 5 cubed is   125, whose sum of digits is  8, which is NOT odd.
     """
     # -------------------------------------------------------------------------
-    # TODO: 3. Implement and test this function.
+    # DONE: 3. Implement and test this function.
     #          Tests have been written for you (above).
     #
     ###########################################################################
@@ -261,6 +268,11 @@ def run_test_problem3b():
 
 
 def problem3b(m, r):
+    total=0
+    for k in range(m):
+        total=(k+1)/((r+k)**(k+1))+total
+    return total
+
     """
     What comes in:  A positive integer m and a number r.
     What goes out:
